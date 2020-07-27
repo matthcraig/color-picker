0d037866aba21df4e68ab318d21de6cf5770bdfd
Matthew Craig <mgcraig78@gmail.com>
2020-07-27 15:36:49 -0700

    refactor js and display rgb values in body

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,9 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
-color1.addEventListener("input", function(){
+function gradientPicker(){
   body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-});
-color2.addEventListener("input", function(){
-    body.style.background = "linear-gradient(to right, " + color2.value + ", " + color1.value + ")";
-  });
-
+  css.textContent = body.style.background + ";"
+}
+color1.addEventListener("input", gradientPicker);
+color2.addEventListener("input", gradientPicker);
